4d36f78bca74cd8c27a6ff1bcb975069da2ef4c2
Oleg <kakhnych.oleg@gmail.com>
2022-11-12 13:22:54 +0300

    update mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,19 @@ function onEntry(entry) {
    observer.observe(elm);
  }
 
+
+
+ $('.mobile div').on('click', () => {
+  $('.mobile div').toggleClass('active');
+  $('.mobile nav').toggleClass('open');
+  $('.mobile nav ul').toggleClass('show');
+});
+for (let a = 1; a <= $(".mobile ul li").length; a++){
+  $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");   
+}
+
+
+
  $('.reviews-slider').slick({
   dots: true,
   infinite: true,
